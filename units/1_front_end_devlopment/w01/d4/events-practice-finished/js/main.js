42defc5898bbabe0afa9d822906073f76dc6634c
Victor <victorluo920@gmail.com>
2020-03-12 14:58:55 -0700

    committed work

---
+++ units/1_front_end_devlopment/w01/d4/events-practice-finished/js/main.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector('button');
+btn.addEventListener('click', function(evt) {
+    const li = document.createElement('li');
+    const inp = document.querySelector('input');
+    li.textContent = inp.value
+    document.querySelector('ul').appendChild(li)
+ });
+
+ document.querySelector('ul')
+   .addEventListener('click', handleClick);
+ 
+ function handleClick(evt) {
+   console.log(evt);
+ }
