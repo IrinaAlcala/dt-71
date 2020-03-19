7b3e3c3fed29236844a4d55609c2a9c48e7b7d01
Irina Alcala <irinaalcala@Jasons-MBP.fios-router.home>
2020-03-19 16:40:32 -0700

    add tic tac toe

---
+++ units/1_front_end_devlopment/w01/d4/events-practice/main.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('button');
+btn.addEventListener('click', function(evt) {
+    const li = document.createElement('li');
+
+  });
+ btn.addEventListener('click', function(evt) {
+   // testing!
+   console.log(evt);  
+ });
