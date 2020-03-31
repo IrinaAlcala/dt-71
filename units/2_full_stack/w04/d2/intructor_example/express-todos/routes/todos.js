6d7d123f12569f0e8d3de06ece8b911e08a606c0
Fred Ngo <fredngo@gmail.com>
2020-03-31 13:51:29 -0400

    Show the todos

--- units/2_full_stack/w04/d2/intructor_example/express-todos/routes/todos.js
+++ units/2_full_stack/w04/d2/intructor_example/express-todos/routes/todos.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const Todo = require('../models/todo');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('todos/index', { todos: Todo.getAll() });
 });
 
 module.exports = router;
