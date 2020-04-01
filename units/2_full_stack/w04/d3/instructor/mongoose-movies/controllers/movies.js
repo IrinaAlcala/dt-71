012e054a601eeaee3d34eb364bf5b836fbcbb870
Fred Ngo <fredngo@gmail.com>
2020-04-01 14:56:40 -0400

    Add ability to actually save the movie

--- units/2_full_stack/w04/d3/instructor/mongoose-movies/controllers/movies.js
+++ units/2_full_stack/w04/d3/instructor/mongoose-movies/controllers/movies.js
@@ -5,7 +5,29 @@ const newMovie = (req, res) => {
 }
 
 const create = (req, res) => {
-  console.log('Creating a new movie!!');
+
+  // convert nowShowing's checkbox of nothing or "on" to boolean
+  req.body.nowShowing = !!req.body.nowShowing;
+  // remove whitespace next to commas
+  req.body.cast = req.body.cast.replace(/\s*,\s*/g, ',');
+  // split if it's not an empty string
+  if (req.body.cast) req.body.cast = req.body.cast.split(',');
+  // remove empty properties
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key];
+  }
+
+  const movie = new Movie(req.body);
+  movie.save(function(err) {
+    // one way to handle errors
+    if (err)
+      return res.render('movies/new');
+    else {
+      console.log(movie);
+      res.redirect('/movies');
+    }
+  });
+  
 }
 
 module.exports = {
