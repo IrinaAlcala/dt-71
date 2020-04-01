1f5204ccf556947001e606bfb76219ffe6ef5762
Fred Ngo <fredngo@gmail.com>
2020-04-01 14:38:50 -0400

    Implement views/movies/new.ejs

--- units/2_full_stack/w04/d3/instructor/mongoose-movies/controllers/movies.js
+++ units/2_full_stack/w04/d3/instructor/mongoose-movies/controllers/movies.js
@@ -4,6 +4,11 @@ const newMovie = (req, res) => {
   res.render('movies/new');
 }
 
+const create = (req, res) => {
+  console.log('Creating a new movie!!');
+}
+
 module.exports = {
+  create,
   new: newMovie
 }
--- units/2_full_stack/w04/d3/instructor/mongoose-movies/routes/movies.js
+++ units/2_full_stack/w04/d3/instructor/mongoose-movies/routes/movies.js
@@ -4,5 +4,6 @@ var router = express.Router();
 const moviesCtrl = require('../controllers/movies');
 
 router.get('/new', moviesCtrl.new);
+router.post('/', moviesCtrl.create);
 
 module.exports = router;
