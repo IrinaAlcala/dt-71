8cdf25785c55e8f7d50f44ffc5f20feab9b7de00
Fred Ngo <fredngo@gmail.com>
2020-04-06 14:07:11 -0400

    move auth into headers

--- units/2_full_stack/w05/d1/instructor/github-users/routes/index.js
+++ units/2_full_stack/w05/d1/instructor/github-users/routes/index.js
@@ -8,9 +8,16 @@ const accessToken = "171061b9b15ed55d552303ba2a4b8ff25e4740ad" // BAD!!!
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const username = req.query.username;
+
+  const options = {
+    url: `${rootURL}/users/${username}?access_token=${accessToken}`,
+    headers: { 
+      'User-Agent': 'fredngo'
+    }
+  };
   
   request(
-    `${rootURL}/users/${username}?access_token=${accessToken}`,
+    options,
     (err, response, body) => {
       console.log(body);
       res.render('index');
