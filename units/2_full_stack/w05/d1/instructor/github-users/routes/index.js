6ed075b63a132eab1d14e403365fe697983bc69c
Fred Ngo <fredngo@gmail.com>
2020-04-06 14:07:05 -0400

    Successfully sent request to github

--- units/2_full_stack/w05/d1/instructor/github-users/routes/index.js
+++ units/2_full_stack/w05/d1/instructor/github-users/routes/index.js
@@ -2,12 +2,21 @@ const express = require('express');
 const router = express.Router();
 const request = require('request');
 
+const rootURL = 'https://api.github.com';
+const accessToken = "171061b9b15ed55d552303ba2a4b8ff25e4740ad" // BAD!!!
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const username = req.query.username;
-  console.log(`username: ${username}`);
+  
+  request(
+    `${rootURL}/users/${username}?access_token=${accessToken}`,
+    (err, response, body) => {
+      console.log(body);
+      res.render('index');
+    }
+  );
 
-  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
