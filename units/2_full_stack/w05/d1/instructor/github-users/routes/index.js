8a4b3849450d0c0c6f47fc18c74df89b594158b7
Fred Ngo <fredngo@gmail.com>
2020-04-06 13:15:03 -0400

    log the username

--- units/2_full_stack/w05/d1/instructor/github-users/routes/index.js
+++ units/2_full_stack/w05/d1/instructor/github-users/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const username = req.query.username;
+  console.log(`username: ${username}`);
+
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+// when processing POST requests, use req.params
+// when processing GET requests, use req.query
