5c8b03b7a4022c5f20264f2eb79251d5065993ea
Fred Ngo <fredngo@gmail.com>
2020-04-07 15:01:49 -0400

    in-class code for w05/d2

---
+++ units/2_full_stack/w05/d2/01-js-promises/starter-code/mongoose-movies-instructor/seeds.js
@@ -0,0 +1,31 @@
+require('./config/database');
+
+const Movie = require('./models/movie');
+const Performer = require('./models/performer');
+const data = require('./data');
+
+const mPromise = Movie.deleteMany({});
+const pPromise = Performer.deleteMany({});
+
+Promise.all([mPromise, pPromise]).then( () => {
+  console.log('Everything Deleted!');
+
+  return Promise.all([
+    Performer.create(data.performers), 
+    Movie.create(data.movies)
+  ]);
+}).then( (result) => {
+  console.log('Movies and Performers are created!');
+  
+  return Promise.all([
+    Performer.findOne({ name: 'Mark Hamill' }),
+    Movie.findOne({ title: 'Star Wars - A New Hope' })
+  ]);
+}).then( (result) => {
+  mark = result[0];
+  sw = result[1];
+  sw.cast.push(mark);
+  return sw.save();
+}).then( () => {
+  process.exit();
+});
