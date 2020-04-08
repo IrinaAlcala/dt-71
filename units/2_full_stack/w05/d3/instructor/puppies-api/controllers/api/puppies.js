f1d1b0379e5f372b187c5970b8943786342a477e
Fred Ngo <fredngo@gmail.com>
2020-04-08 15:04:24 -0400

    Implement api puppies controller

--- units/2_full_stack/w05/d3/instructor/puppies-api/controllers/api/puppies.js
+++ units/2_full_stack/w05/d3/instructor/puppies-api/controllers/api/puppies.js
@@ -1,10 +1,34 @@
 const Puppy = require('../../models/puppy');
 
-const index = (req, res) => {};
-const show = (req, res) => {};
-const create = (req, res) => {};
-const update = (req, res) => {};
-const deleteOne = (req, res) => {};
+const index = (req, res) => {
+  Puppy.find({}).then( (puppies) => {
+    res.status(200).json(puppies);
+  });
+};
+
+const show = (req, res) => {
+  Puppy.findById(req.params.id).then( (puppy) => {
+    res.status(200).json(puppy)
+  });
+};
+
+const create = (req, res) => {
+  Puppy.create(req.body).then( (puppy) => { 
+    res.status(201).json(puppy);  
+  });
+};
+
+const update = (req, res) => {
+  Puppy.findByIdAndUpdate(req.params.id, req.body, { new: true }).then( (puppy) => {
+    res.status(200).json(puppy);
+  });
+};
+
+const deleteOne = (req, res) => {
+  Puppy.findByIdAndRemove(req.params.id).then( (puppy) => {
+    res.status(200).json(puppy);
+  });
+};
 
 module.exports = {
   index,
