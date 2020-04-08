8a8929889cc55e572f458ab6841029f97b6c23cc
Fred Ngo <fredngo@gmail.com>
2020-04-08 13:41:36 -0400

    Add apiPuppies router and controller

---
+++ units/2_full_stack/w05/d3/instructor/puppies-api/controllers/api/puppies.js
@@ -0,0 +1,15 @@
+const Puppy = require('../../models/puppy');
+
+const index = (req, res) => {};
+const show = (req, res) => {};
+const create = (req, res) => {};
+const update = (req, res) => {};
+const deleteOne = (req, res) => {};
+
+module.exports = {
+  index,
+  show,
+  create,
+  update,
+  delete: deleteOne
+};
